4d9b87e5ba66318caf1c54f612925c473a73c88b
Csm Mohasin Alam <mohasinalam@aol.com>
2026-01-25 20:01:25 +0600

    Update sw.js

--- new/sw.js
+++ new/sw.js
@@ -1,7 +1,41 @@
-const cacheName = 'notepad-v1';
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(['./', './index.html', './manifest.json'])));
+const CACHE_NAME = 'ghost-shell-v3-cache';
+const ASSETS_TO_CACHE = [
+    '/new/',
+    '/new/index.html',
+    '/new/manifest.json',
+    'https://fonts.googleapis.com/css2?family=Fira+Code:wght@400;600&display=swap',
+    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+    'https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js',
+    'https://www.gstatic.com/firebasejs/8.10.0/firebase-auth.js',
+    'https://www.gstatic.com/firebasejs/8.10.0/firebase-database.js'
+];
+
+// Service Worker Install হলে ফাইলগুলো ক্যাশ করবে
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Caching assets...');
+            return cache.addAll(ASSETS_TO_CACHE);
+        })
+    );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+
+// পুরনো ক্যাশ ডিলিট করার জন্য (ভার্সন আপডেট হলে)
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
+
+// নেটওয়ার্ক না থাকলে ক্যাশ থেকে ফাইল সার্ভ করবে
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
