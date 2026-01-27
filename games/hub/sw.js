f86e1c516eb36b56d4420d9e334713c455dcfad7
Csm Mohasin Alam <mohasinalam@aol.com>
2026-01-28 00:21:41 +0600

    Create sw.js

---
+++ games/hub/sw.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'neon-hub-v1';
+const ASSETS = [
+    './',
+    './index.html'
+];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.map((key) => {
+                if (key !== CACHE_NAME) return caches.delete(key);
+            }));
+        })
+    );
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
+});
